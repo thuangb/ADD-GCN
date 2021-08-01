e7a4137e6ae3080ce9f099131dde2c1bafb55a8f
thunanguyen <thuan.nguyenthuanz123@hcmut.edu.vn>
2021-08-01 16:22:44 +0700

    add support for Res2Net pretrained models

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,7 @@
 import torch
 import torchvision
 from .add_gcn import ADD_GCN
+from .res2net_v1b import res2net101_v1b
 
 model_dict = {'ADD_GCN': ADD_GCN}
 
@@ -12,6 +13,8 @@ def get_model(num_classes, args):
         model_name = 'resnext101_32x4d_swsl'
 
         pretrain = torch.hub.load(model_url, model_name)
+    elif args.pretrain_name == 'res2net101-v1b':
+        pretrain = res2net101_v1b(pretrained=True)
     else:
         raise Exception('Pretrained models not supported')
 
